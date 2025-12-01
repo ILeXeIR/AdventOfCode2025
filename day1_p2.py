336f148da69a4762fda93c92fa83b122dc1180f7
Alexander Petukhov <alexander.petukhov@my.games>
2025-12-01 13:37:51 +0100

    Day 1 puzzle 2 solved

--- day1_p2.py
+++ day1_p2.py
@@ -4,7 +4,7 @@ class Solution:
             return [self.get_number(line.strip()) for line in file.readlines()]
 
     def get_number(self, line: str) -> int:
-        num  = int(line[1:]) % 100
+        num  = int(line[1:])
         return num if line[0] == "R" else -num
     
     def find_solution(self, filename: str) -> int:
@@ -12,13 +12,16 @@ class Solution:
         point = 50
         nulls = 0
         for move in moves:
+            if point == 0 and move < 0:
+                nulls -= 1
             point += move
-            if point < 0:
-                point += 100
-            elif point > 99:
-                point -= 100
             if point == 0:
                 nulls += 1
+            elif point < 0:
+                nulls -= (point - 1) // 100
+            else:
+                nulls += point // 100
+            point %= 100
         return nulls
 
 if __name__ == "__main__":
